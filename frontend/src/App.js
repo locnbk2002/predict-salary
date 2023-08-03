cda265ca5943b4959afc315056001384269520b6
dungnguyen2110 <dungnguyen912002@gmail.com>
2023-08-03 16:21:14 +0700

    Fix ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,49 @@
 import React, { useState, useEffect } from "react";
-import Modal from "@mui/material/Modal";
-import Button from "@mui/material/Button";
+import {
+    Button,
+    Modal,
+    AppBar,
+    Toolbar,
+    Typography,
+    Container,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Box,
+    Grid,
+} from "@mui/material";
 
 import BankHub from "./component/Bankhub";
+import "./App.css";
 
 const server = process.env.REACT_APP_SERVER_URL;
-const TransactionsTable = ({ dataTransaction }) => {
-    return (
-        <div>
-            <h2>Danh sách giao dịch:</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>STT</th>
-                        <th>Ngày giao dịch</th>
-                        <th>Số tiền</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataTransaction.map((transaction, index) => (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{transaction.transactionDate}</td>
-                            <td>{transaction.amount}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+const containerStyle = {
+    backgroundColor: "#f8f8f8",
+    flex: "1",
+    padding: "50px",
+};
+
+const textContainerStyle = {
+    backgroundColor: "#fff",
+    borderRadius: "8px",
+    border: "1px solid #ccc",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    textAlign: "center",
+    height: "100%",
+    padding: "50px",
+    boxSizing: "border-box",
+};
+
+const buttonStyle = {
+    marginTop: "16px",
+    minWidth: "150px",
 };
 const App = () => {
+    // Mo owraa aa  aaa
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     //   Biến showw iframe
@@ -64,6 +77,8 @@ const App = () => {
     const [isDonate, setIsDonate] = useState(false);
 
     function handleGetTransaction() {
+        setLoadingData(true);
+
         let accessToken = localStorage.getItem("accessToken");
         return fetch(`${server}/api/transactions`, {
             method: "POST",
@@ -75,6 +90,8 @@ const App = () => {
             body: JSON.stringify({ accessToken, isDonate }),
         })
             .then((response) => {
+                setLoadingData(false);
+
                 if (response.status === 404) {
                     // toast.error(
                     //     "Chúng tôi không thể lấy được giao dịch của bạn. Vui lòng thử lại sau",
@@ -108,6 +125,8 @@ const App = () => {
             });
     }
 
+    console.log(isModalOpen);
+
     function handleExchangeTokenAndGetData(publicToken) {
         fetch(`${server}/api/grant_exchange`, {
             method: "POST",
@@ -121,17 +140,14 @@ const App = () => {
             .then((response) => response.json())
             .then((data) => {
                 if (!data.error) {
-                    setLoadingData(true);
-
                     setDataTransactions({});
 
                     localStorage.setItem("accessToken", data.accessToken);
-                } else {
-                    setLoadingData(false);
                 }
 
                 handleGetTransaction();
             });
+        setIsModalOpen(false);
 
         setShow(false);
     }
@@ -171,46 +187,212 @@ const App = () => {
     };
 
     return (
-        <div>
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={handleOpenModal}
-            >
-                Chạy thử thuật toán
-            </Button>
-            {show ? <BankHub src={link} /> : null}
-            <Modal open={isModalOpen} onClose={handleCloseModal}>
-                <div
-                    style={{
-                        backgroundColor: "#fff",
-                        padding: 20,
-                        borderRadius: 8,
-                    }}
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+            }}
+        >
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">DuDoanLuong.com</Typography>
+                </Toolbar>
+            </AppBar>
+
+            <Grid container style={containerStyle}>
+                <Grid
+                    sx={{ border: "1px solid #ccc", background: "#fff" }}
+                    item
+                    xs={8}
                 >
-                    <h2>Chọn một trong hai lựa chọn:</h2>
-                    {/* <Button onClick={() => handleRunAlgorithm("contribute")}> */}
-                    <Button onClick={() => handleBanklink(true)}>
-                        Đóng góp
-                    </Button>
-                    <Button
-                        // onClick={() => handleRunAlgorithm("not-contribute")}
-                        onClick={() => handleBanklink(false)}
-                    >
-                        Không
-                    </Button>
-                </div>
-            </Modal>
-            {/* Hiển thị dữ liệu trả về dưới dạng danh sách */}
-            <div>Lương dự đoán: {salary}</div>
-            <div>Ngày cuối cùng nhận lương: {lastestSalary}</div>
-            <div>
-                {dataTransactions.length > 0 ? (
-                    <TransactionsTable dataTransaction={dataTransactions} />
-                ) : (
-                    ""
-                )}
-            </div>
+                    {show ? <BankHub src={link} /> : null}
+                    <Modal open={isModalOpen} onClose={handleCloseModal}>
+                        <div
+                            style={{
+                                top: "50%",
+                                left: "50%",
+                                transform: "translate(-50%, -50%)",
+                                position: "absolute",
+                                backgroundColor: "#fff",
+                                padding: 20,
+                                borderRadius: 8,
+                            }}
+                        >
+                            <Typography variant="h5">
+                                Chọn một trong hai lựa chọn:
+                            </Typography>
+                            <Box
+                                mt={2}
+                                display="flex"
+                                justifyContent="space-between"
+                            >
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={() => handleBanklink(true)}
+                                >
+                                    Đóng góp
+                                </Button>
+                                <Button
+                                    variant="contained"
+                                    onClick={() => handleBanklink(false)}
+                                >
+                                    Không
+                                </Button>
+                            </Box>
+                        </div>
+                    </Modal>
+                    {dataTransactions.length > 0 ? (
+                        <>
+                            <Box sx={{ paddingLeft: "20px" }} mt={3}>
+                                <Typography
+                                    sx={{
+                                        textAlign: "center",
+                                        fontSize: "33px",
+                                        marginBottom: "15px",
+                                        fontWeight: "700",
+                                        color: "blue",
+                                    }}
+                                    variant="h2"
+                                >
+                                    Kết quả dự đoán
+                                </Typography>
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        flexDirection: "row",
+                                    }}
+                                >
+                                    <Typography
+                                        variant="h6"
+                                        sx={{ flex: "50%" }}
+                                    >
+                                        <strong>Lương dự đoán: </strong>
+                                        <i>{salary.toLocaleString()}</i>
+                                    </Typography>
+
+                                    <Typography
+                                        variant="h6"
+                                        sx={{ flex: "50%" }}
+                                    >
+                                        <strong>Số tháng nhận lương </strong>
+                                        <i>{monthSalary}</i>
+                                    </Typography>
+                                </Box>
+                                <Box sx={{ marginTop: "10px" }}>
+                                    <Typography variant="h6">
+                                        <strong>
+                                            Ngày nhận lương gần nhất:{" "}
+                                        </strong>
+                                        <i>{lastestSalary}</i>
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            {dataTransactions.length > 0 && (
+                                <Box mt={3}>
+                                    <TableContainer
+                                        component={Paper}
+                                        style={{
+                                            maxHeight: 300,
+                                            overflowY: "auto",
+                                            padding: "0 20px",
+                                            width: "100%",
+                                            boxSizing: "border-box",
+                                        }}
+                                    >
+                                        <Table>
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell>STT</TableCell>
+                                                    <TableCell>
+                                                        Ngày giao dịch
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        Số tiền
+                                                    </TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {dataTransactions.map(
+                                                    (transaction, index) => (
+                                                        <TableRow key={index}>
+                                                            <TableCell>
+                                                                {index + 1}
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {
+                                                                    transaction.transactionDate
+                                                                }
+                                                            </TableCell>
+                                                            <TableCell>
+                                                                {
+                                                                    transaction.amount
+                                                                }
+                                                            </TableCell>
+                                                        </TableRow>
+                                                    )
+                                                )}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Box>
+                            )}
+                        </>
+                    ) : (
+                        ""
+                    )}
+                </Grid>
+                <Grid item xs={4}>
+                    <Box style={textContainerStyle}>
+                        <Typography variant="h5" gutterBottom>
+                            Chào bạn,
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            Chúng tôi đang phát triển thuật toán để dự đoán
+                            lương tự động dựa vào lịch sử giao dịch của tài
+                            khoản ngân hàng thông qua
+                            <strong> BankHub</strong>. Để thuật toán chính xác
+                            hơn, chúng tôi cần một lượng lớn dữ liệu để cải
+                            thiện thuật toán của mình.
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <strong>
+                                Sự đóng góp của bạn là một sự giúp đỡ to lớn với
+                                chúng tôi.
+                            </strong>
+                        </Typography>
+                        <Typography variant="body1" gutterBottom>
+                            <strong>Người thực hiện:</strong> Nguyễn Văn Dũng,
+                            Búi Tiến Lộc.
+                        </Typography>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            style={buttonStyle}
+                            onClick={handleOpenModal}
+                            className={`${loadingData ? "lds_hourglass" : ""}`}
+                            disabled={loadingData}
+                        >
+                            {loadingData ? "Đang xử lý" : "Chạy thử thuật toán"}
+                        </Button>
+                    </Box>
+                </Grid>
+            </Grid>
+            <footer
+                style={{
+                    marginTop: "auto",
+                    backgroundColor: "#f0f0f0",
+                    padding: 20,
+                }}
+            >
+                <Container maxWidth="md">
+                    <Typography variant="body1" align="center">
+                        © {new Date().getFullYear()} DuDoanLuong. All rights
+                        reserved.
+                    </Typography>
+                </Container>
+            </footer>
         </div>
     );
 };
